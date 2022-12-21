bf255288ecc604446275eb46d90064885404f0d0
Chamroeun OUM <chamroeunoum@gmail.com>
2022-12-21 21:30:19 +0700

    Finish module YP Cambodia completed.  Please enter the commit message for your changes. Lines starting

--- app/Console/Commands/YPReader.php
+++ app/Console/Commands/YPReader.php
@@ -30,8 +30,9 @@ class YPReader extends Command
     {
         echo 'Start reading' . PHP_EOL ;
         $categoryCounter = 0 ;
-        for( $i = 1 ; $i <=12 ; $i++ ) {
-            $xpathResult = YPReaderController::readPageWithCurl( "https://ypkhmer.com/category/subcategory/all?page=".$i , "//div[@class='col-md-6 col-12']/a[@class='section-b']" );
+        $totalChilds = 0 ;
+        for( $categoryPage = 1 ; $categoryPage <=12 ; $categoryPage++ ) {
+            $xpathResult = YPReaderController::readPageWithCurl( "https://ypkhmer.com/category/subcategory/all?page=".$categoryPage , "//div[@class='col-md-6 col-12']/a[@class='section-b']" );
             if( $xpathResult == false ) continue;
             foreach($xpathResult as $dlsIndex => $dl ) {
                 $categoryCounter++;
@@ -44,9 +45,9 @@ class YPReader extends Command
                 $categoryObj = \App\Models\YPCategory::where('name', str_replace([" ","\n","•"],[""],$dl->childNodes[1]->textContent ) )->first() ;
                 if( $categoryObj === null ){
                     $categoryObj = \App\Models\YPCategory::create([
-                        'name' => str_replace([" ","\n","•"],[""],$dl->childNodes[1]->textContent )
+                        'name' => trim( str_replace(["\n","•"],[""],$dl->childNodes[1]->textContent ) )
                     ]);
-                    echo( $categoryCounter . ' : ' . $categoryObj->name . ' has been saved.' . PHP_EOL ) ;
+                    echo( 'Page : ' . $categoryPage . ', Category : ' . $categoryCounter . ' : ' . $categoryObj->name . ' has been saved.' . PHP_EOL ) ;
                 }
 
                 /**
@@ -56,24 +57,19 @@ class YPReader extends Command
                 if( $dataItems == false ) continue;
                 $totalPage = $dataItems[ $dataItems->length - 2 ] == null ? 1 : $dataItems[ $dataItems->length - 2 ]->textContent ;
                 $childCounter = 0 ;
-                for($i=1; $i<=$totalPage;$i++){
-                    $items = YPReaderController::readPageWithCurl( $dl->attributes[0]->value . "?page=" . $i , "//div[@class='card-body p-0 pl-1 h-100 min-h-150']" );
+                for($page=1; $page<=$totalPage;$page++){
+                    $items = YPReaderController::readPageWithCurl( $dl->attributes[0]->value . "?page=" . $page , "//div[@class='card-body p-0 pl-1 h-100 min-h-150']" );
                     if( $items == false ) continue;
                     foreach( $items AS $itemIndex => $item ){
                         $childCounter++;
-                        // $category['childs'][] = [
-                        //     'name' => str_replace([" ","\n","•"],[""], $item->childNodes[1]->textContent ) ,
-                        //     'category' => str_replace([" ","\n","•"],[""], $item->childNodes[5]->textContent ) ,
-                        //     'phone' => explode('/',str_replace([" ","\n","•"],[""], $item->childNodes[7]->textContent ) ) ,
-                        //     'address' => explode(',',str_replace([" ","\n","•"],[""], $item->childNodes[9]->textContent ) )
-                        // ];
+                        $totalChilds++;
                         $ypProduct = \App\Models\YPProduct::create([
-                            'name' => str_replace([" ","\n","•"],[""], $item->childNodes[1]->childNodes[1]->childNodes[1]->textContent ) ,
+                            'name' => trim( str_replace(["\n","•"],[""], $item->childNodes[1]->childNodes[1]->childNodes[1]->textContent ) ),
                             'category' => $categoryObj->id ,
                             'phone' => str_replace([" ","\n","•"],[""], $item->childNodes[7]->textContent ) ,
                             'address' => str_replace([" ","\n","•"],[""], $item->childNodes[9]->textContent )
                         ]);                        
-                        echo( $categoryCounter . ' : ' . $categoryObj->name . ', ' . $childCounter . ' => ' . $ypProduct->name . PHP_EOL ) ;
+                        echo( "No : " . $totalChilds . ' = Page : ' . $categoryPage . ', Category : ' . $categoryCounter . ' : ' . $categoryObj->name . ', ChildPage : ' . $page . ' : ' . $childCounter . ' => ' . $ypProduct->name . PHP_EOL ) ;
                     }
                 }
             }
