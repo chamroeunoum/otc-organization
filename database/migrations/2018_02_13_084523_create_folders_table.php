832d91e6c59d3b9c12e5f873eadc27b6da48618a
chamroeunoum <chamroeunoum@gmail.com>
2023-04-26 14:52:10 +0700

    Fixed problem with pid field within folders migration

--- database/migrations/2018_02_13_084523_create_folders_table.php
+++ database/migrations/2018_02_13_084523_create_folders_table.php
@@ -17,7 +17,7 @@ class CreateFoldersTable extends Migration {
 			$table->increments('id');
 			$table->string('name', 191)->nullable();
 			$table->integer('user_id')->unsigned()->index('document_folders_user_id_foreign');
-			$table->integer('pid',11)->nullable(true)->comment('The parent id of the folder');
+			$table->integer('pid')->unsigned()->comment('The parent id of the folder');
 			$table->integer('active',1)->default(0)->comment('The status of the folder to be used or disabled.');
 			$table->timestamps();
 			$table->softDeletes();
