f9a6fe24b05fb5b61d69b35ae6eb9ccde1399554
chamroeunoum <chamroeunoum@gmail.com>
2023-04-26 14:49:29 +0700

    Fixed problem with changing name of the migration file types to document_types

--- database/migrations/2018_02_13_084140_create_document_types_table.php
+++ database/migrations/2018_02_13_084140_create_document_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateTypesTable extends Migration {
+class CreateDocumentTypesTable extends Migration {
 
 	/**
 	 * Run the migrations.
