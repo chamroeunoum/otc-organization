b1fb25494ba04d1312b8950c34343d8b212273c2
chamroeunoum <chamroeunoum@gmail.com>
2023-09-09 16:24:01 +0700

    Update user seeding to add role to each user as backend

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,6 +17,72 @@ class UsersTableSeeder extends Seeder
         
 
         \DB::table('users')->delete();
+
+        /**
+         * Write the dumpmy data for the application
+         */
+
+        /**
+         * Create role for the user
+         */
+        $super = \App\Models\Role::create(['name' => 'Super Administrator', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $administrator = \App\Models\Role::create(['name' => 'Administrator', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $backendMember = \App\Models\Role::create(['name' => 'Backend member', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $client = \App\Models\Role::create(['name' => 'Client', 'guard_name' => 'api' , 'tag' => 'webapp']);
+
+        /**
+         * Create accounts
+         */
+        /**
+         * Create admin user for development purpose
+         */
+        $chamroeunoum = \App\Models\User::create([
+            'firstname' => 'Chamroeun' ,
+            'lastname' => 'OUM' ,
+            'email' => 'chamroeunoum@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('031884Chamroeunoum') ,
+            'phone' => '012557200' ,
+            'username' => 'chamroeunoum'
+        ]);
+        
+        $people = \App\Models\People\People::create([
+            'firstname' => $chamroeunoum->firstname , 
+            'lastname' => $chamroeunoum->lastname , 
+            'gender' => 0 , // Male
+            'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
+            'mobile_phone' => $chamroeunoum->phone , 
+            'email' => $chamroeunoum->email
+        ]);
+        $chamroeunoum->people_id = $people->id ;
+        $chamroeunoum->save();
+        $chamroeunoum->assignRole( $administrator );
+
+
+        /**
+         * Create client user for development purpose
+         */
+        $puthireach = \App\Models\User::create([
+            'firstname' => 'Puthireach' ,
+            'lastname' => 'KONGCHAN' ,
+            'email' => 'kongchanputhireach@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('1234567890+1') ,
+            'phone' => '010517515' ,
+            'username' => 'kcputhireach'
+        ]);
+        
+        $people = \App\Models\People\People::create([
+            'firstname' => $puthireach->firstname , 
+            'lastname' => $puthireach->lastname , 
+            'gender' => 0 , // Male
+            'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
+            'mobile_phone' => $puthireach->phone , 
+            'email' => $puthireach->email
+        ]);
+        $puthireach->people_id = $people->id ;
+        $puthireach->save();
+        $puthireach->assignRole( $client );
         
         \DB::table('users')->insert(array (
             0 => 
@@ -651,6 +717,9 @@ class UsersTableSeeder extends Seeder
             ),
         ));
         
+        \App\Models\User::where('id','>',4)->get()->map(function($user) use( $backendMember ){
+            $user->assignRole( $backendMember )
+        });
         
     }
 }
