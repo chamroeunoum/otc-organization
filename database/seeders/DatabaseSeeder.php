c06ab5be1ead323496c3a4e75de2ef765ea9a165
chamroeunoum <chamroeunoum@gmail.com>
2024-03-25 23:06:48 +0700

    Update Seeding file to run only the development data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,129 +16,154 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        // /**
-        //  * Create role for the user
-        //  */
-        // $super = \App\Models\Role::create(['name' => 'super', 'guard_name' => 'api' , 'tag' => 'core_service']);
-        // $administrator = \App\Models\Role::create(['name' => 'admin', 'guard_name' => 'api' , 'tag' => 'core_service']);
-        // $backendMember = \App\Models\Role::create(['name' => 'backend', 'guard_name' => 'api' , 'tag' => 'core_service']);
-        // $client = \App\Models\Role::create(['name' => 'client', 'guard_name' => 'api' , 'tag' => 'client_service']);
+        /**
+         * Create role for the user
+         */
+        $super = \App\Models\Role::create(['name' => 'super', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $administrator = \App\Models\Role::create(['name' => 'admin', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $backendMember = \App\Models\Role::create(['name' => 'backend', 'guard_name' => 'api' , 'tag' => 'core_service']);
+        $client = \App\Models\Role::create(['name' => 'client', 'guard_name' => 'api' , 'tag' => 'client_service']);
 
-        // /**
-        //  * Create accounts
-        //  */
-        // /**
-        //  * Create admin user for development purpose
-        //  */
-        // $chamroeunoum = \App\Models\User::create([
-        //     'firstname' => 'Chamroeun' ,
-        //     'lastname' => 'OUM' ,
-        //     'email' => 'chamroeunoum@gmail.com' ,
-        //     'active' => 1 ,
-        //     'password' => bcrypt('031884Chamroeunoum') ,
-        //     'phone' => '012557200' ,
-        //     'username' => 'chamroeunoum'
-        // ]);
+        /**
+         * Create accounts
+         */
+        /**
+         * Create admin user for development purpose
+         */
+        $chamroeunoum = \App\Models\User::create([
+            'firstname' => 'Chamroeun' ,
+            'lastname' => 'OUM' ,
+            'email' => 'chamroeunoum@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('031884Chamroeunoum') ,
+            'phone' => '012391848' ,
+            'username' => 'chamroeunoum'
+        ]);
         
-        // $people = \App\Models\People\People::create([
-        //     'firstname' => $chamroeunoum->firstname , 
-        //     'lastname' => $chamroeunoum->lastname , 
-        //     'gender' => 0 , // Male
-        //     'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
-        //     'mobile_phone' => $chamroeunoum->phone , 
-        //     'email' => $chamroeunoum->email
-        // ]);
-        // $chamroeunoum->people_id = $people->id ;
-        // $chamroeunoum->save();
-        // $chamroeunoum->assignRole( $administrator );
+        $people = \App\Models\People\People::create([
+            'firstname' => $chamroeunoum->firstname , 
+            'lastname' => $chamroeunoum->lastname , 
+            'gender' => 0 , // Male
+            'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
+            'mobile_phone' => $chamroeunoum->phone , 
+            'email' => $chamroeunoum->email
+        ]);
+        $chamroeunoum->people_id = $people->id ;
+        $chamroeunoum->save();
+        $chamroeunoum->assignRole( $supser );
 
-        // /**
-        //  * Create client user for development purpose
-        //  */
-        // $puthireach = \App\Models\User::create([
-        //     'firstname' => 'Puthireach' ,
-        //     'lastname' => 'KONGCHAN' ,
-        //     'email' => 'kongchanputhireach@gmail.com' ,
-        //     'active' => 1 ,
-        //     'password' => bcrypt('1234567890+1') ,
-        //     'phone' => '010517515' ,
-        //     'username' => 'kcputhireach'
-        // ]);
+        /**
+         * Create admin user for development purpose
+         */
+        $rosachan = \App\Models\User::create([
+            'firstname' => 'Rosa' ,
+            'lastname' => 'CHAN' ,
+            'email' => 'rosthika@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('031884Chamroeunoum') ,
+            'phone' => '017391848' ,
+            'username' => 'rosachan'
+        ]);
         
-        // $people = \App\Models\People\People::create([
-        //     'firstname' => $puthireach->firstname , 
-        //     'lastname' => $puthireach->lastname , 
-        //     'gender' => 0 , // Male
-        //     'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
-        //     'mobile_phone' => $puthireach->phone , 
-        //     'email' => $puthireach->email
-        // ]);
-        // $puthireach->people_id = $people->id ;
-        // $puthireach->save();
-        // $puthireach->assignRole( $backendMember );
+        $people = \App\Models\People\People::create([
+            'firstname' => $rosachan->firstname , 
+            'lastname' => $rosachan->lastname , 
+            'gender' => 1 , // Male
+            'dob' => \Carbon\Carbon::parse('1983-02-23 9:00') ,
+            'mobile_phone' => $rosachan->phone , 
+            'email' => $rosachan->email
+        ]);
+        $rosachan->people_id = $people->id ;
+        $rosachan->save();
+        $rosachan->assignRole( $administrator );
 
-        // /**
-        //  * Create client user for development purpose
-        //  */
-        // $bellamudhita = \App\Models\User::create([
-        //     'firstname' => 'Bella Mudhita' ,
-        //     'lastname' => 'KONGCHAN' ,
-        //     'email' => 'kongchanbellamudhita@gmail.com' ,
-        //     'active' => 1 ,
-        //     'password' => bcrypt('1234567890+1') ,
-        //     'phone' => '010517515' ,
-        //     'username' => 'kcbellamudhita'
-        // ]);
+        /**
+         * Create client user for development purpose
+         */
+        $puthireach = \App\Models\User::create([
+            'firstname' => 'Puthireach' ,
+            'lastname' => 'KONGCHAN' ,
+            'email' => 'kongchanputhireach@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('1234567890+1') ,
+            'phone' => '012557200' ,
+            'username' => 'kcputhireach'
+        ]);
         
-        // $people = \App\Models\People\People::create([
-        //     'firstname' => $bellamudhita->firstname , 
-        //     'lastname' => $bellamudhita->lastname , 
-        //     'gender' => 0 , // Male
-        //     'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
-        //     'mobile_phone' => $bellamudhita->phone , 
-        //     'email' => $bellamudhita->email
-        // ]);
-        // $bellamudhita->people_id = $people->id ;
-        // $bellamudhita->save();
-        // $bellamudhita->assignRole( $client );
+        $people = \App\Models\People\People::create([
+            'firstname' => $puthireach->firstname , 
+            'lastname' => $puthireach->lastname , 
+            'gender' => 0 , // Male
+            'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
+            'mobile_phone' => $puthireach->phone , 
+            'email' => $puthireach->email
+        ]);
+        $puthireach->people_id = $people->id ;
+        $puthireach->save();
+        $puthireach->assignRole( $backendMember );
 
-        $this->call(TagsTableSeeder::class);
-
-        $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserRoleTableSeeder::class);
-
-        $this->call(PeopleTableSeeder::class);
-        $this->call(PeoplePositionsTableSeeder::class);
-        $this->call(PeopleCountesiesTableSeeder::class);
-
-        $this->call(TimeslotsTableSeeder::class);
-        $this->call(UserTimeslotsTableSeeder::class);
+        /**
+         * Create client user for development purpose
+         */
+        $bellamudhita = \App\Models\User::create([
+            'firstname' => 'Bella Mudhita' ,
+            'lastname' => 'KONGCHAN' ,
+            'email' => 'kongchanbellamudhita@gmail.com' ,
+            'active' => 1 ,
+            'password' => bcrypt('1234567890+1') ,
+            'phone' => '010517515' ,
+            'username' => 'kcbellamudhita'
+        ]);
         
-        $this->call(OrganizationLeaderTableSeeder::class);
-        $this->call(OrganizationPeopleTableSeeder::class);
+        $people = \App\Models\People\People::create([
+            'firstname' => $bellamudhita->firstname , 
+            'lastname' => $bellamudhita->lastname , 
+            'gender' => 0 , // Male
+            'dob' => \Carbon\Carbon::parse('1984-03-18 9:00') ,
+            'mobile_phone' => $bellamudhita->phone , 
+            'email' => $bellamudhita->email
+        ]);
+        $bellamudhita->people_id = $people->id ;
+        $bellamudhita->save();
+        $bellamudhita->assignRole( $client );
 
-        $this->call(MeetingsTableSeeder::class);
-        $this->call(MeetingRoomsTableSeeder::class);
-        $this->call(MeetingOrganizationsTableSeeder::class);
-        $this->call(MeetingMembersTableSeeder::class);
-        $this->call(MeetingAttendantsTableSeeder::class);
+        // $this->call(TagsTableSeeder::class);
 
-        $this->call(RegulatorsTableSeeder::class);
-        $this->call(RegulatorTypesTableSeeder::class);
-        $this->call(RegulatorSignaturesTableSeeder::class);
-        $this->call(OrganizationRegulatorsTableSeeder::class);
-        $this->call(OrganizationOwnRegulatorsTableSeeder::class);
-        $this->call(OrganizationRelatedRegulatorsTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(UserRoleTableSeeder::class);
+
+        // $this->call(PeopleTableSeeder::class);
+        // $this->call(PeoplePositionsTableSeeder::class);
+        // $this->call(PeopleCountesiesTableSeeder::class);
+
+        // $this->call(TimeslotsTableSeeder::class);
+        // $this->call(UserTimeslotsTableSeeder::class);
+        
+        // $this->call(OrganizationLeaderTableSeeder::class);
+        // $this->call(OrganizationPeopleTableSeeder::class);
+
+        // $this->call(MeetingsTableSeeder::class);
+        // $this->call(MeetingRoomsTableSeeder::class);
+        // $this->call(MeetingOrganizationsTableSeeder::class);
+        // $this->call(MeetingMembersTableSeeder::class);
+        // $this->call(MeetingAttendantsTableSeeder::class);
+
+        // $this->call(RegulatorsTableSeeder::class);
+        // $this->call(RegulatorTypesTableSeeder::class);
+        // $this->call(RegulatorSignaturesTableSeeder::class);
+        // $this->call(OrganizationRegulatorsTableSeeder::class);
+        // $this->call(OrganizationOwnRegulatorsTableSeeder::class);
+        // $this->call(OrganizationRelatedRegulatorsTableSeeder::class);
 
         // About the content of the regulator
-        $this->call(BooksTableSeeder::class);
-        $this->call(KuntiesTableSeeder::class);
-        $this->call(MatikasTableSeeder::class);
-        $this->call(ChaptersTableSeeder::class);
-        $this->call(PartsTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
-        $this->call(MatrasTableSeeder::class);
+        // $this->call(BooksTableSeeder::class);
+        // $this->call(KuntiesTableSeeder::class);
+        // $this->call(MatikasTableSeeder::class);
+        // $this->call(ChaptersTableSeeder::class);
+        // $this->call(PartsTableSeeder::class);
+        // $this->call(SectionsTableSeeder::class);
+        // $this->call(MatrasTableSeeder::class);
         
 
         /**
