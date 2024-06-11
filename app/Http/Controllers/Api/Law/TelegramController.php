70b820cf526cb042e16d3b755f4da4e32f2ffcb6
chamroeunoum <chamroeunoum@gmail.com>
2024-06-11 14:01:45 +0700

    Fixing eror because of null value in table people.firstname

--- app/Http/Controllers/Api/Law/TelegramController.php
+++ app/Http/Controllers/Api/Law/TelegramController.php
@@ -124,13 +124,13 @@ class TelegramController extends Controller
             if( $user->people_id > 0 && ( $person = \App\Models\People\People::find( $user->people_id )->onlyTrashed()->first() ) != null ){
                 $person->restore();
                 $person->update([
-                    'firstname' => $user->firstname ,
-                    'lastname' => $user->lastname
+                    'firstname' => $user->firstname??$user->telegram_user_firstname ,
+                    'lastname' => $user->lastname??$user->telegram_user_lastname
                 ]);
             }else{
                 $person = \App\Models\People\People::create([
-                    'firstname' => $user->firstname ,
-                    'lastname' => $user->lastname
+                    'firstname' => $user->firstname??$user->telegram_user_firstname ,
+                    'lastname' => $user->lastname??$user->telegram_user_lastname
                 ]);
             }
             $user->people_id = $person->id;
